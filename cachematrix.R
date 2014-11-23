d58a4f7589f4507252ab085e115ffe35ee618c92
sn0g9e <kung_schuh@hotmail.com>
2014-11-23 20:20:11 +0100

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+makeCacheMatrix <- function(x = matrix()) {  # input x will be a matrix
+  inv <- NULL #set to NULL each time makeCacheMatrix() is called
   set <- function(y) {
-    x <<- y
-    inv <<- NULL
+    #superassignment_
+    x <<- y 
+    inv <<- NULL #if matrix is re-assigned it invalidates the previously computed inverse.
   }
-  get <- function() x
+  get <- function() x #returns the value of the original vector
+  
+  #this is called by cacheSolve() during the first cacheSolve()
+  #access and it will store the value using superassignment
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
+  
+  #invoke the list function which returns a list. 
+  #accessed each time makeCacheMatrix() is called, 
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+# Computes, caches, and returns a matrix inverse
+cacheSolve <- function(x, ...) { # the input x is an object created by makeCacheMatrix()
+  # Return a matrix that is the inverse of 'x'
+  # can be called with amatrix$getinverse()
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data.")
     return(inv)
   }
+  # get() can be called from 'outside' and returns 'original' matrix
   data <- x$get()
   inv <- solve(data)
   x$setinverse(inv)
